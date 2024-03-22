b47e3024cc8e89bfde7f588030b20700bdf8e0d5
Khemraj <khemraj@gmail.com>
2024-03-22 16:51:42 +0530

    Update code

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,66 +1,66 @@
 class Api::V1::ArticlesController < ApplicationController
   def index
-    #Display all record..
+    # Display all records
     articles = Article.all
     render json: articles, status: 200
   end
 
   def show
-    #Get data by id.. 
-    articles = Article.find_by(id: params[:id]);
-    if articles
-      render json: articles, status:200
+    # Get record data by id
+    article = Article.find_by(id: params[:id])
+    if article
+      render json: article, status: 200
     else
-      render json:'Article not found';
+      render json: { error: 'Record not found' }, status: 404
     end
-
   end
 
   def create
-    #Create user..
+    # Create article
     article = Article.new(
       title: arti_params[:title],
       body: arti_params[:body],
       author: arti_params[:author]
     )
     if article.save
-      render json: article, status: 200  # Mistake: Missing colon after "status"
+      render json: article, status: 201
     else
-      render json: {
-       error: "there is some error"  # Mistake: Using colon instead of hash rocket
-      }  
+      render json: { error: 'There is some error' }, status: 422
     end
   end
 
   def destroy
+    # Delete article record
     article = Article.find_by(id: params[:id])
     if article
-      if article.delete
-        render json: { message: "Data delete successfully!" }, status:200
+      if article.destroy
+        render json: { message: 'Data deleted successfully!' }, status: 200
       else
-        render json: { error: "Record could not be deletd" }, status: 500
+        render json: { error: 'Record could not be deleted' }, status: 500
       end
     else
-      render json: { error: "Record not found" }, status: 500
+      render json: { error: 'Record not found' }, status: 404
     end
   end
 
   def update
+    # Update article data
     article = Article.find_by(id: params[:id])
     if article
       if article.update(title: params[:title], body: params[:body], author: params[:author])
-        render json: { message: "Data updated successfully!" }, status:200
+        render json: { message: 'Data updated successfully!' }, status: 200
       else
-        render json: { error: "Record could not be updated" }, status: 500
+        render json: { error: 'Record could not be updated' }, status: 500
       end
     else
-      render json: { error: "Record not found" }, status: 500
+      render json: { error: 'Record not found' }, status: 404
     end
   end
 
   private
+
   def arti_params
-    params.require(:article).permit(  # Mistake: Using square brackets instead of parentheses
+    params.require(:article).permit(
       :title,
       :body,
       :author
